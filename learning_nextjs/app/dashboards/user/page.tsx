f4d388c9d0fd541565811cf8ed006f48928bd303
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-12-03 19:50:32 +0530

    Jsut take in Dynamic routing

--- learning_nextjs/app/dashboards/user/page.tsx
+++ learning_nextjs/app/dashboards/user/page.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Link from 'next/link'
 
 const user = () => {
   return (
-    <div>Its a user page</div>
+    <div>
+        <h1>dashboard</h1>
+
+        <ul>
+            <li><Link href= "/dashboard/user/1"> User 1</Link></li>
+            <li><Link href="/dashboard/user/2"> User 2</Link> </li>
+            <li><Link href="/dashboard/user/3"> User 3</Link></li>
+            <li><Link href="/dashboard/user/4"> User 4</Link></li>
+            <li><Link href="/dashboard/user/5"> User 5</Link></li>
+        </ul>
+    </div>
   )
 }
 
