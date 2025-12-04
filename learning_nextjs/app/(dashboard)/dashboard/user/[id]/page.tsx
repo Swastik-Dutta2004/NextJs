b2c88e258ba45373702bc271a087315f36ef9299
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-12-04 20:42:24 +0530

    Learn dynamic routing inside the page routing

--- learning_nextjs/app/(dashboard)/dashboard/user/[id]/page.tsx
+++ learning_nextjs/app/(dashboard)/dashboard/user/[id]/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const page = async ({Params}: {Params: Promise <{id: string}>}) => {
-  const {id} = await Params
+const page = async ({params}: {params: Promise <{id: string}>}) => {
+  const {id} = await params
   return (
     <div>
       <h1>Showing the details of user# {id}</h1>
