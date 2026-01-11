8dac58bbd74a086bd48115c8499bd3ae1335ba07
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2026-01-11 12:32:47 +0530

    Starting to make project

--- learning/app/(root)/about/page.tsx
+++ learning/app/(root)/about/page.tsx
@@ -1,6 +1,8 @@
+import { error } from 'console'
 import React from 'react'
 
 const About = () => {
+  // throw new Error
   return (
     <div>It is about page</div>
   )
---
+++ project/app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='text-9xl'>welcome fuckers</div>
+  )
+}
+
+export default page
