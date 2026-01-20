75941c7d0161553554c57f4becef35afaac5ace0
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2026-01-20 12:18:51 +0530

    setuping cloudinay for images.

--- project/app/api/events/route.ts
+++ project/app/api/events/route.ts
@@ -1,27 +1,47 @@
-// import connectDB from "@/lib/mongodb";
+import connectDB from "@/lib/mongodb";
 import { NextRequest, NextResponse } from "next/server";
 import Event from "@/database/event.model"
+import { promises } from "dns";
+import {v2 as cloudinary} from 'cloudinary'
 
 export async function POST(req: NextRequest) {
     try {
-        // await connectDB();
+        await connectDB();
 
-        const fordata = await req.formData();
+        const fordata = await req.json();
 
         let event;
         try {
-            event = Object.fromEntries(fordata.entries())
+            
+            // fordata is already a plain object, no need to convert
+            event = fordata;
+            
         } catch (e) {
-            return NextResponse.json({message: "Invaild JSON format"}, {status: 400})
+            return NextResponse.json({message: "Invalid JSON format"}, {status: 400})
         }
+
+        const file = fordata.get('image') as File;
+        if (!file) {
+            return NextResponse.json({message: "Iamge file is required"},{status: 400})
+
+            const ArrayBuffer = await file.arrayBuffer();
+            const buffer = Buffer.from(ArrayBuffer)
+
+            const uplodeFile = await new Promise((resolve, reject) => {
+                
+            })
+        }
+
+
+
 
         const createEvents = await Event.create(event);
         
-        return NextResponse.json({message: "Events create successfully", event: createEvents}, {status: 201})
+        return NextResponse.json({message: "Events created successfully", event: createEvents}, {status: 201})
 
     } catch (e) {
         console.error(e);
-        return NextResponse.json({message: "Event creation falied", error: e instanceof Error ? e.message : "Unknown"}, {status: 500})
+        return NextResponse.json({message: "Event creation failed", error: e instanceof Error ? e.message : "Unknown"}, {status: 500})
         
     }
 }
