331eb95e3896a374f00ca3ad88e113de58117f8d
Swastik Dutta <duttaswastik004@gmail.com>
2026-03-11 21:08:04 +0530

    Add a new file

--- Scrimba/app/layout.jsx
+++ Scrimba/app/layout.jsx
@@ -1,7 +1,9 @@
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  )
+export default function Rootlayout({ children }) {
+    return (
+        <html lang="en">
+            <body>
+                {children}
+            </body>
+        </html>
+    )
 }
